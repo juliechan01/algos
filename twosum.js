7b8ceab9cecd6e91c0ad02181e3d793d5ada2cd3
Julie Chan <juliechan03@gmail.com>
2023-09-02 12:13:09 -0700

    0(n) time complexity solution of twosum algo

--- twosum.js
+++ twosum.js
@@ -11,3 +11,24 @@ var twoSum = function(nums, target) {
         }
     }
 };
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = function(nums, target) {
+    const hashmap = new Map(); // Creating the hash map
+
+    for(i = 0; i < nums.length; i++) {
+        let value = target - nums[i]; // Set difference to target value to see if they match when looping through hashmap
+
+        // Refer back to array & check to see if array has solution
+        if (hashmap.has(value)) {
+            return [hashmap.get(value), i];
+        }
+
+        // Return indices that create target value from array
+        hashmap.set(nums[i], i);
+    }
+};
